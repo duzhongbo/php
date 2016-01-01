e47549b00ee8d3d475ed0b390e13520b3ee9af42
duzhongbo <710409599@qq.com>
2016-01-01 21:42:04 +0800

    前端通过接口传值后端，后端返回格式化结果

--- 3.php
+++ 3.php
@@ -1,16 +1,46 @@
 <?php
 
-// $a = json_encode(array('a'=>'bbb','c'=>'ddd'));
+if(isset($_POST["username"])&&$_POST["password"]&&$_POST["email"])
+{
+ $user=$_POST["username"];
+ $age=$_POST["password"];
+ $email=$_POST["email"];
 
+$servername = "localhost";
+$username = "root";
+$password = "654321";
+$dbname = "blog";
+
+// 创建连接
+$conn = new mysqli($servername, $username, $password, $dbname);
+// 检测连接
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO user (username, password, email)
+VALUES ('$user', '$age', '$email')";
+
+if ($conn->query($sql) === TRUE) {
+    echo json_encode(array('status'=>1,'data'=>'{}','msg'=>'success'));
+} else {
+    echo "<br><br>Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+} 
+
+
+
+
+// $username = $_POST["username"];
+// $password = $_POST["password"];
+// $email = $_POST["email"];
+
+// $a = json_encode(array('username'=>$username,'password'=>$password,'email'=>$email));
 // echo $a;
 
 
-$username = $_POST["username"];
-$password = $_POST["password"];
-$email = $_POST["email"];
-
-$a = json_encode(array('username'=>$username,'password'=>$password,'email'=>$email));
-echo $a;
 ?>
 
 
