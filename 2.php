5b90754461ce21441ae9d8ec100b5d1f627abeac
duzhongbo <710409599@qq.com>
2016-01-01 21:28:00 +0800

    表单提交数据，提交php接口，接口返回json数据

--- 2.php
+++ 2.php
@@ -1,42 +1,59 @@
-<form action="?" method="post">
-username: <input type="text" name="username"><br>
-password: <input type="text" name="password"><br>
-email: <input type="text" name="email"><br>
-<input type="submit">
-</form>
+
+username: <input type="text" name="username" id="username"><br>
+password: <input type="text" name="password" id="password"><br>
+email: <input type="text" name="email" id="email"><br>
+<input type="button" onclick="submit()">
+
+
+<script type="text/javascript" src="zepto.min.js"></script>
+<script type="text/javascript">
+	
+function submit(){
+	$.post('3.php',{
+		username:username.value,
+		password:password.value,
+		email:email.value,
+	},function(json){
+		data = JSON.parse(json)
+		console.log(data);
+	})
+}
+
+
+</script>
 
 <?php
-if(isset($_POST["username"])&&$_POST["password"]&&$_POST["email"])
-{
- $user=$_POST["username"];
- $age=$_POST["password"];
- $email=$_POST["email"];
- echo $user;
- echo $age;
- echo $email;
-$servername = "localhost";
-$username = "root";
-$password = "654321";
-$dbname = "blog";
+// if(isset($_POST["username"])&&$_POST["password"]&&$_POST["email"])
+// {
+//  $user=$_POST["username"];
+//  $age=$_POST["password"];
+//  $email=$_POST["email"];
+//  echo $user;
+//  echo $age;
+//  echo $email;
+// $servername = "localhost";
+// $username = "root";
+// $password = "654321";
+// $dbname = "blog";
 
-// 创建连接
-$conn = new mysqli($servername, $username, $password, $dbname);
-// 检测连接
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+// // 创建连接
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// // 检测连接
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
 
-$sql = "INSERT INTO user (username, password, email)
-VALUES ('$user', '$age', '$email')";
+// $sql = "INSERT INTO user (username, password, email)
+// VALUES ('$user', '$age', '$email')";
 
-if ($conn->query($sql) === TRUE) {
-    echo "<br><br>New record created successfully";
-} else {
-    echo "<br><br>Error: " . $sql . "<br>" . $conn->error;
-}
+// if ($conn->query($sql) === TRUE) {
+//     echo "<br><br>New record created successfully";
+// } else {
+//     echo "<br><br>Error: " . $sql . "<br>" . $conn->error;
+// }
 
-$conn->close();
-} 
+// $conn->close();
+// } 
 
 ?>
 
--- 3.php
+++ 3.php
@@ -1,7 +1,15 @@
 <?php
 
-$a = json_encode(array('a'=>'bbb','c'=>'ddd'));
+// $a = json_encode(array('a'=>'bbb','c'=>'ddd'));
 
+// echo $a;
+
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+$email = $_POST["email"];
+
+$a = json_encode(array('username'=>$username,'password'=>$password,'email'=>$email));
 echo $a;
 ?>
 
